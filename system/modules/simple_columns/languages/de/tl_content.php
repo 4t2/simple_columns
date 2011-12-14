cf7fe50cb2905351efb5e80e543e47975e5a9ced
Mario Müller <mario@lingo4u.com>
2011-12-14 12:33:19 +0100

    English translation added

--- system/modules/simple_columns/languages/de/tl_content.php
+++ system/modules/simple_columns/languages/de/tl_content.php
@@ -8,21 +8,20 @@ $GLOBALS['TL_LANG']['tl_content']['simple_columns_legend'] = 'Spalteneinstellung
 /**
  * References
  */
- $GLOBALS['TL_LANG']['tl_content']['simple_columns']['title'] = array('Spalten', 'Anzahl der Spalten');
- $GLOBALS['TL_LANG']['tl_content']['simple_columns']['reference'] = array(
- 	'2' => '2-spaltig',
- 	'3' => '3-spaltig',
- 	'3-2' => '→ über 2 Spalten',
- 	'4' => '4-spaltig',
+$GLOBALS['TL_LANG']['tl_content']['simple_columns']['title'] = array('Spalten', 'Anzahl der Spalten');
+$GLOBALS['TL_LANG']['tl_content']['simple_columns']['reference'] = array(
+	'2' => '2-spaltig',
+	'3' => '3-spaltig',
+	'3-2' => '→ über 2 Spalten',
+	'4' => '4-spaltig',
 	'4-2' => '→ über 2 Spalten',
 	'4-3' => '→ über 3 Spalten',
- 	'5' => '5-spaltig',
+	'5' => '5-spaltig',
 	'5-2' => '→ über 2 Spalten',
 	'5-3' => '→ über 3 Spalten',
 	'5-4' => '→ über 4 Spalten'
- );
- 
- $GLOBALS['TL_LANG']['tl_content']['simple_columns_close'] = array('Zeile manuell schließen', 'Zeile manuell schließen und nächste Spalte in einer neuen Zeile anfangen.');
- 
- 
- ?>
+);
+
+$GLOBALS['TL_LANG']['tl_content']['simple_columns_close'] = array('Zeile manuell schließen', 'Zeile manuell schließen und nächste Spalte in einer neuen Zeile anfangen.');
+
+?>
---
+++ system/modules/simple_columns/languages/en/tl_content.php
@@ -0,0 +1,27 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_content']['simple_columns_legend'] = 'Column settings';
+ 
+/**
+ * References
+ */
+$GLOBALS['TL_LANG']['tl_content']['simple_columns']['title'] = array('Columns', 'Number of columns');
+$GLOBALS['TL_LANG']['tl_content']['simple_columns']['reference'] = array(
+	'2' => '2 columns',
+	'3' => '3 columns',
+	'3-2' => '→ span 2 columns',
+	'4' => '4 columns',
+	'4-2' => '→ span 2 columns',
+	'4-3' => '→ span 3 columns',
+	'5' => '5 columns',
+	'5-2' => '→ span 2 columns',
+	'5-3' => '→ span 3 columns',
+	'5-4' => '→ span 4 columns'
+);
+
+$GLOBALS['TL_LANG']['tl_content']['simple_columns_close'] = array('Force close column', 'Close the column and start next on new row.');
+
+?>
